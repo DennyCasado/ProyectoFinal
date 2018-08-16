4b59808411f0871b74965f2a4c3fb4ce8de00339
DennyCasado <dennycasado17@gmail.com>
2018-08-16 15:57:19 -0400

    Subida final del proyecto

---
+++ CalculoNomina/CalculoNomina/Nomina.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoNomina
+{
+    public class Nomina
+    {
+        private int diasLaborados;
+
+        public int DiasLaborados  { get; set; }
+
+        //metodo para calcular la nomina
+        public decimal CalculoNomina (int diasLab, decimal valorDia)
+        {
+            decimal totalSalario = diasLab * valorDia;
+            return totalSalario;
+        
+        }
+
+    }
+}
